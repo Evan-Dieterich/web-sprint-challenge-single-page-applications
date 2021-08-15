0dbe703f81dc29e78a9f3ba0a8686569f47b5617
Evan Dieterich <evan.dieterich.az@gmail.com>
2021-08-15 14:53:45 -0700

    MVP Complete

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import Form from "./Forms/Form";
+import Formmenu from "./Forms/Formmenu";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
-const App = () => {
+export default function App() {
+  
   return (
-    <>
+    <div className="App">
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Form Form={Form} />
+          </Route>
+          <Route path="/Formmenu/:id">
+            <Formmenu Formmenu={Formmenu} />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
   );
-};
-export default App;
+}
